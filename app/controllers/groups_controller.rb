03c2f653265637239edd518e71c21e4e3e549505
mateomh <mmojicah.84@gmail.com>
2020-09-21 11:08:27 -0500

    Add alphabetical order in the groups index view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
   # GET /groups
   # GET /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.all.ordered_by_name
   end
 
   # GET /groups/1
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   has_many :groupedtransactions
   has_many :expenses, through: :groupedtransactions
   belongs_to :creator, class_name: 'User'
+  scope :ordered_by_name, -> { order(:name) }
 end
