724c9eba0b858add86566146a0a82f74f96f78bf
mateomh <mmojicah.84@gmail.com>
2020-09-21 15:05:22 -0500

    Add scope to expense model to get the stand alone expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,4 +4,5 @@ class Expense < ApplicationRecord
   has_many :groupedtransactions
   has_many :groups, through: :groupedtransactions
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  scope :stand_alone_expenses, -> { where('id NOT IN (?)', Groupedtransaction.all.ids) }
 end
