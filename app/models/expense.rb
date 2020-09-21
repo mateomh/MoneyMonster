108cc50e1765ee5e9a4be6bac8df48dd06d3943b
mateomh <mmojicah.84@gmail.com>
2020-09-21 18:47:27 -0500

    Update the expenses model for the stand alone expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,8 +1,8 @@
 class Expense < ApplicationRecord
   validates :name, :amount, presence: true
   belongs_to :author, class_name: 'User'
-  has_many :groupedtransactions
+  has_many :groupedtransactions, dependent: :destroy
   has_many :groups, through: :groupedtransactions
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
-  scope :stand_alone_expenses, -> { where('id NOT IN (?)', Groupedtransaction.pluck(:expense_id)) }
+  scope :stand_alone_expenses, -> { where('id NOT IN (?)', Groupedtransaction.distinct.pluck(:expense_id)) }
 end
