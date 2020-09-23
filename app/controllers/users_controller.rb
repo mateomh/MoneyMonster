8c313e71cb99600e03f505c1be0163ecb1b7617a
mateomh <mmojicah.84@gmail.com>
2020-09-22 21:24:54 -0500

    Add custom alert messages for all the views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
+        format.html { redirect_to new_user_path, alert: 'User already exists' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def navbar_rendering
     render partial: "layouts/navbar" unless session[:user_id].nil?
   end
+
+  def information_rendering
+    render partial: "layouts/info" unless notice.nil? && alert.nil?
+  end
 end
