b6297b2ccc5c29c8e2ed6bb9bb404c90a8c09371
mateomh <mmojicah.84@gmail.com>
2020-09-22 22:57:17 -0500

    Add avatar selection feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to '/users', notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { redirect_to new_user_path, alert: 'User already exists' }
