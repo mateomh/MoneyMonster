e94230240f594c99b9be5c15b124cf81ce412c15
mateomh <mmojicah.84@gmail.com>
2020-09-23 23:16:14 -0500

    Update instance variables for the all model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  User.delete_all
-  Expense.delete_all
-  Group.delete_all
-  Groupedtransaction.delete_all
-  test_u = User.create(name: 'mateo')
-  Expense.create(name: 'test expense', author_id: test_u.id, amount: 50.0)
-  Group.create(name: 'test group', creator_id: test_u.id)
+  before(:each) do
+    User.delete_all
+    Expense.delete_all
+    Group.delete_all
+    Groupedtransaction.delete_all
+    @test_u = User.create(name: 'mateo')
+    Expense.create(name: 'test expense', author_id: @test_u.id, amount: 50.0)
+    Group.create(name: 'test group', creator_id: @test_u.id)
+  end
 
   it '1. Checks for the user name precense validation' do
     u = User.create(name:nil)
@@ -25,14 +27,14 @@ RSpec.describe User, type: :model do
   end
 
   it '4. Checks for a successful user creation' do
-    expect(test_u).to be_valid
+    expect(@test_u).to be_valid
   end
 
   it '5. Checks the association between the expense and user models' do
-    expect(test_u.expenses).not_to be_empty
+    expect(@test_u.expenses).not_to be_empty
   end
 
   it '6. Checks the association between the group and user models' do
-    expect(test_u.groups).not_to be_empty
+    expect(@test_u.groups).not_to be_empty
   end
 end
