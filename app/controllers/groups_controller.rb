ed91ac91db12d10e225ddcb3630233e251191f84
mateomh <mmojicah.84@gmail.com>
2020-09-24 16:50:10 -0500

    Update queries to address the n+1 problem in the groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,13 +5,13 @@ class GroupsController < ApplicationController
   # GET /groups.json
   def index
     @user = User.find(session[:user_id])
-    @groups = @user.groups.all.ordered_by_name
+    @groups = @user.groups.ordered_by_name
   end
 
   # GET /groups/1
   # GET /groups/1.json
   def show
-    @group_expenses = @group.expenses.ordered_by_most_recent
+    @group_expenses = @group.expenses.includes(:groupedtransactions, :groups).ordered_by_most_recent
   end
 
   # GET /groups/new
