70d2aebfe69da2cd307680a7453ec5bb7456381a
mateomh <mmojicah.84@gmail.com>
2020-09-24 16:37:33 -0500

    Update queries to address the n+1 problem in the expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,14 +4,14 @@ class ExpensesController < ApplicationController
   # GET /expenses
   # GET /expenses.json
   def my_external
-    @user = User.find(session[:user_id])
-    @expenses = @user.expenses.stand_alone_expenses.ordered_by_most_recent
+    @user = User.includes(:expenses, :groups).find(session[:user_id])
+    @expenses = @user.expenses.includes(:groups, :author).stand_alone_expenses.ordered_by_most_recent
     render 'index'
   end
 
-  def index
-    @user = User.find(session[:user_id])
-    @expenses = @user.expenses.all.ordered_by_most_recent
+  def index #/mytransactions
+    @user = User.includes(:expenses, :groups).find(session[:user_id])
+    @expenses = @user.expenses.includes(:groups, :author).ordered_by_most_recent
   end
 
   # GET /expenses/1
