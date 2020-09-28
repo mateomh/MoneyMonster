eb766082990ee9eae3f31e6e10c9369192a243ed
mateomh <mmojicah.84@gmail.com>
2020-09-27 21:26:14 -0500

    Add strong parameters to the access control controller

--- app/controllers/access_control_controller.rb
+++ app/controllers/access_control_controller.rb
@@ -1,14 +1,14 @@
 class AccessControlController < ApplicationController
   def signin
-    valid_user = User.where(name: params[:user][:name])
+    valid_user = User.where(user_params)
     if valid_user.empty?
       flash.notice = "User doesn't exists"
       redirect_to users_path
     else
-      session[:user_name] = params[:user][:name]
+      session[:user_name] = user_params[:name]
       session[:user_id] = valid_user[0].id
       session[:avatar] = valid_user[0].avatar_url
-      flash.notice = "Welcome back #{session[:username]}"
+      flash.notice = "Welcome back #{session[:user_name]}"
       redirect_to "/users/#{session[:user_id]}"
     end
   end
@@ -17,5 +17,11 @@ class AccessControlController < ApplicationController
     session.destroy
     flash.notice = 'Logged out successfully'
     redirect_to root_path
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
